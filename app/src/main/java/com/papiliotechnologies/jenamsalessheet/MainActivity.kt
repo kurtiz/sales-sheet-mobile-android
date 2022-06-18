a2d061d31ff9d9e298b127031fdd089a2cea1599
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-06-18 14:47:04 +0000

    Added a bottom navigation bar :lipstick:
    Completely removed the previously added sales activity and its related files and added a new activiy to handle main activity leaving me to rename the previous activty as the splash screen activity

--- app/src/main/java/com/papiliotechnologies/jenamsalessheet/MainActivity.kt
+++ app/src/main/java/com/papiliotechnologies/jenamsalessheet/MainActivity.kt
@@ -1,25 +1,13 @@
 package com.papiliotechnologies.jenamsalessheet
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import android.content.Intent
-import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val window = this.window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = this.resources.getColor(R.color.green)
-        Handler().postDelayed({
-            val salesIntent = Intent(this@MainActivity, SalesActivity::class.java)
-            startActivity(salesIntent)
-            finish()
-        }, 3000)
     }
 }
